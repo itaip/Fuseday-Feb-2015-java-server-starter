f5ff89d574f09ee80a18f7910baac43d88977596
robiferentz <robiferentz@gmail.com>
2015-02-24 14:18:26 +0200

    functional buttons

--- src/main/webapp/scripts/ctrl/MainController.ctrl.js
+++ src/main/webapp/scripts/ctrl/MainController.ctrl.js
@@ -2,7 +2,20 @@
  * Created by robiferentz on 2/24/15.
  */
 angular.module('fuse-team1.controllers')
-.controller('MainController',['$scope',function($scope){
-    $scope.mapMsg
- = 'Hi';
-}]);
+    .controller('MainController', ['$scope', '$http', function ($scope, $http) {
+        "use strict";
+
+        $scope.mapMsg = 'Hi';
+
+        $scope.attack = function(){
+            $http.post('/attack/' + $scope.attackIP + '/10').success(function(res){
+                console.log('Attack started.');
+            });
+        };
+
+        $scope.stop = function(){
+            $http.delete('/attack').success(function(){
+
+            });
+        };
+    }]);
